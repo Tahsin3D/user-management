5ad8b98d5c4f79383dbcfe04ddd903623efe5e15
Tahsin Haider <tahsin3194@gmail.com>
2025-05-09 16:55:47 +0500

    user Registration UI with modal

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,4 +8,28 @@ const loadRegister = async(req, res) => {
     }
 }
 
-export { loadRegister }
+const insertUser = async (req, res) => {
+    try {
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            mobile: req.body.phone,
+            image: req.body.filename,
+            password: req.body.password,
+            is_admin: false,
+        });
+
+        const userData = await user.save();
+
+        if(userData){
+            res.render("registration", {message: "Registration successful."})
+        }
+        else {
+            res.render("registration", {message: "Registration Unsuccessful."})
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export { loadRegister, insertUser }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,9 @@
-import express from "express"
-import {loadRegister} from "../controllers/userController.js"
+import express from "express";
+import { insertUser, loadRegister } from "../controllers/userController.js";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import bodyParser from "body-parser";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,9 +11,12 @@ const __dirname = dirname(__filename);
 const app = express();
 
 app.set("view engine", "ejs");
-
 app.set("views", path.join(__dirname, "../views/layouts/users"));
 
-app.get('/register', loadRegister);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/register", loadRegister);
+app.post("/register", insertUser);
 
 export default app;
